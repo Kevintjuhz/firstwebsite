52cdf8ead37b1ae7f472b78b7103dc652993fdc6
Kevintjuhz <kq@xs4all.nl>
2017-04-06 20:18:06 +0200

    Added some animations

---
+++ animations.js
@@ -0,0 +1,26 @@
+/* Home Page Scroll Reveal */
+
+window.sr = ScrollReveal();
+
+sr.reveal('.service-items', { 
+    origin: 'left',
+    duration: 500, 
+    scale: 0.9,
+    mobile: true,
+    easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+    reset: true,
+    delay: 200,
+    distance: '100px'
+});
+
+sr.reveal('.testimonials', { 
+    origin: 'left',
+    duration: 200, 
+    distance: '100px',
+    delay: 100,
+    scale: 0.9,
+    mobile: true,
+    easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+    reset: true,
+    viewFactor: 0.4
+});
